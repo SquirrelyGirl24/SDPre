6ca6c93ec103e38db377cb1e24f74f33d4951e34
SquirrelyGirl24 <donna.krutka@outlook.com>
2021-04-19 17:34:32 -0600

    complete Objective3Lab1.java

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 15.0;
+    num2 = num1 + 6.0;
+    num3 = num2 + 2.0;
+    average = num3 /= 3;
+    System.out.println (average);
+  }
+}
