279413129c68a75a74e2a1bdfa2eaa0b490d5085
SquirrelyGirl24 <donna.krutka@outlook.com>
2021-04-21 14:49:49 -0600

    finish Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    double num1 = Math.sqrt(10 * 10);
+    double num2 = Math.sqrt(8 * 8);
+    double result = Math.sqrt(10) + Math.sqrt(8);
+    hypotenuse = result;
+    System.out.println (hypotenuse);
+
+  }
+}
