f6c629b92eb0ccbd8fb07162b7379297a9947273
SquirrelyGirl24 <donna.krutka@outlook.com>
2021-04-22 17:21:29 -0600

    finish Objective4Lab3.java

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+
+    int birthYear = 0;
+    int currentYear = 2021;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    int age = input.nextInt();
+    birthYear = currentYear - age;
+    System.out.println("You were born in " + birthYear);
+
+  }
+}
